73d769e40c6d34ada03180f2c406cb114ce3915f
Hubert Kasinski <hubertkasinski17@gmail.com>
2025-05-10 06:28:14 +0100

    Add project files.

---
+++ BondPricing.cpp
@@ -0,0 +1,12 @@
+﻿// BondPricing.cpp : Defines the entry point for the application.
+//
+
+#include "BondPricing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
